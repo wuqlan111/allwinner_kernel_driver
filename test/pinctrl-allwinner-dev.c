c4b9a02b1d830bf5e902481056923526b0fe89a3
wuqlan111 <wuqlan111@163.com>
2023-12-08 22:11:46 +0800

    Add pinctrl dev test code.

---
+++ test/pinctrl-allwinner-dev.c
@@ -0,0 +1,96 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/interrupt.h>
+#include <linux/syscore_ops.h>
+#include <linux/err.h>
+#include <linux/clk.h>
+#include <linux/io.h>
+#include <asm-generic/io.h>
+#include <linux/device.h>
+#include <linux/pm_runtime.h>
+#include <linux/pm.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/platform_device.h>
+#include <linux/pinctrl/pinctrl.h>
+#include <linux/pinctrl/pinconf-generic.h>
+#include <linux/pinctrl/pinmux.h>
+#include <linux/pinctrl/pinconf.h>
+#include <linux/bitops.h>
+#include <linux/compiler-gcc.h>
+#include <linux/delay.h>
+
+#include "dbg_log.h"
+
+
+
+static struct resource  dev_res[]  =  {
+    // {
+    //     .name  = DEVICE_PHY_ADDR_RESOURCE,
+    //     .flags =  IORESOURCE_MEM,
+    //     .start = ALLWINNER_CCU_BASE_ADDR, 
+    //     .end = ALLWINNER_CCU_BASE_ADDR + ALLWINNER_CCU_MAP_SIZE,
+    // },
+
+};
+
+
+static void allwinner_pinctrl_dev_release(struct device * dev)
+{
+    _PRINTF_INFO("release dev -- %s\n", dev_name(dev));
+}
+
+
+static struct platform_device  allwinner_pinctrl_dev =  {
+    .name  =  "allwinner_pinctrl_driver",
+    .id  =  PLATFORM_DEVID_NONE,
+    // .resource  =  dev_res,
+    .num_resources  =  0,
+    .dev  =  { 
+        .release  = allwinner_pinctrl_dev_release,
+    },
+};
+
+
+static int32_t  __init  allwinner_pinctrl_dev_init(void)
+{
+    int32_t ret =  0;
+
+    struct device * dev  = &allwinner_pinctrl_dev.dev;
+
+    ret = platform_device_register(&allwinner_pinctrl_dev);
+    if (ret) {
+        _PRINTF_ERROR("register clk dev failed! ret = %d\n", ret);
+        return  ret;
+    } else {
+        _PRINTF_INFO("register allwinner clk dev success!\n");
+    }
+
+    // ret = device_add_groups(dev, clk_dev_test_groups);
+    if (ret) {
+        _PRINTF_ERROR("add device attribute failed! ret = %d\n", ret);
+        platform_device_unregister(&allwinner_clk_dev);
+    }
+
+    return  ret;
+
+}
+
+
+static void  __exit  allwinner_pinctrl_dev_exit(void)
+{
+    platform_device_unregister(&allwinner_clk_dev);
+    _PRINTF_INFO("allwinner pinctrl dev remove!\n");
+
+}
+
+
+module_init(allwinner_clk_dev_init);
+module_exit(allwinner_clk_dev_exit);
+
+MODULE_DESCRIPTION("allwinner's soc pinctrl device");
+MODULE_LICENSE("GPL v2");
+
+
+
+
